349c0402a7fbbdc60a61f193fa451628b49b4fc9
RNsth <aryanshretha1212@gmail.com>
2026-01-09 10:01:40 +0545

    email sending

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib import messages
-
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import ContactForm
 def base_view(request):
     return render(request, 'main/base.html') 
@@ -13,15 +14,36 @@ def about_view(request):
     return render(request, 'main/about.html')
 
 def contact_view(request):
-    if request.method=='POST':
-        form=ContactForm(request.POST)
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
 
-            messages.success(request, 'Thank you for your message! We will get back to you soon.')
-            form = ContactForm() 
+            full_message = f"""
+            Name: {name}
+            Email: {email}
+
+            Message:
+            {message}
+            """
+
+            send_mail(
+                subject,
+                full_message,
+                settings.DEFAULT_FROM_EMAIL,
+                ['aryan@blondmail.com'],  
+                fail_silently=False,
+            )
+
+            messages.success(request, "Your message has been sent successfully!")
+            form = ContactForm()  
+
     else:
         form = ContactForm()
-    return render(request, 'main/contact.html', {'form': form})
+
+    return render(request, "main/contact.html", {"form": form})
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%g&u6wvaal&1#ng2&c0sn7phexl$9rb)s^orm!u415xtm%f0b_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,6 +28,21 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load variables from .env
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Application definition
 
 INSTALLED_APPS = [
